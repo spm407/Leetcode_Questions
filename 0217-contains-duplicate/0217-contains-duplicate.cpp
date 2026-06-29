162b4dfda1c58afced2d8bd3584d9dcd2cf8e1cd
Prince Savaliya <savaliyaprincem@gmail.com>
2026-06-29 17:43:20 +0530

    Time: 87 ms (11.43%), Space: 113.5 MB (5.54%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]=1;
+        }
+        return !(mpp.size()==n);
+    }
+};
